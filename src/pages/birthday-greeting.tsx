95900cd2e9ad18aea81f7398020949781642e489
Vunguye1 <s364540@oslomet.no>
2024-02-04 18:56:11 +0100

    add birthday card

--- src/pages/birthday-greeting.tsx
+++ src/pages/birthday-greeting.tsx
@@ -1,5 +1,41 @@
-const BirthdayGreeting = () => {
-  return <div>Happy birthday</div>;
+import React from "react";
+import bgCSS from "./birthday-greeting.module.css"; // practice using css module
+
+const BirthdayGreeting = (props: any) => {
+  return (
+    <div>
+      <html lang="en">
+        <head>
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <title>haha</title>
+        </head>
+        <body>
+          <div className={bgCSS.container}>
+            <div className={bgCSS.birthdayCard}>
+              <div className={bgCSS.cardcover}>
+                <div className={bgCSS.cardcoverfront}>
+                  <p>Happy birthday, {props.name}</p>
+                </div>
+                <div className={bgCSS.cardcoverback}></div>
+              </div>
+              <div className={bgCSS.cardcontent}>
+                <p>
+                  A wish for you on your birthday, whatever you ask may you
+                  receive, whatever you seek may you find, whatever you wish may
+                  it be fulfilled on your birthday and always.
+                </p>
+                <p> Happy birthday!</p>
+              </div>
+            </div>
+          </div>
+        </body>
+      </html>
+    </div>
+  );
 };
 
 export default BirthdayGreeting;
